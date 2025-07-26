7ecccab774a1fdf33020e976f44d390f08432520
mmiskatul <masabimiskat@gmail.com>
2025-07-26 09:35:57 +0600

    uncomplete

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,18 @@ import cors from 'cors'
 import 'dotenv/config'
 import { connecTotDb } from './config/db.js';
 import userRouter from './routes/userRoutes.js';
+import path from 'path'
+import { fileURLToPath } from 'url';
 
 
+const _filename =fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);       
+// Serve static files from the "uploads" directory
+const uploadsPath = path.join(_dirname, 'uploads');
+const staticMiddleware = express.static(uploadsPath);
+const app = express();
+app.use('/uploads', staticMiddleware);
 // 
-const app=express();
 const PORT =process.env.PORT || 4000;
 
 app.use(cors())
@@ -18,6 +26,7 @@ connecTotDb()
 app.use(express.json());
 
 app.use('/api/auth',userRouter);
+
 
 // ROUTE
 app.get('/',(req,res)=>{
